3624ba29bfc7ca32b65b16a81d2d5d4d30d401d5
bapdai <97323194+bapdai@users.noreply.github.com>
2022-07-11 11:52:40 +0700

    jwt

---
+++ src/main/java/jwt/example/jwtspringboot/dto/JwtResponse.java
@@ -0,0 +1,21 @@
+package jwt.example.jwtspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+}
